c3da5c4a213fc9a2eeeaea7a905a44f2ea4239e2
40293084 <sfulton09@qub.ac.uk>
2023-10-18 21:37:57 +0100

    checkpoint using node

---
+++ Human-Bias/server/config.js
@@ -0,0 +1,3 @@
+const googleClientID='1011280470420-sbgbbes073p7bkvlg2glcnnu572f4o3o.apps.googleusercontent.com'
+const googleClientSecret='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'
+const googleOauthRedirectURL='http://localhost:8080/sessions/oauth/google'
