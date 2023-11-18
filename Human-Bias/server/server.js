01b0dcf6bc761d26f6da61591c03b9b28283e1de
40293084 <sfulton09@qub.ac.uk>
2023-11-18 17:52:49 +0000

    node fetch

--- Human-Bias/server/server.js
+++ Human-Bias/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import fetch from 'node-fetch';
 import bodyParser from 'body-parser';
 import { google } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library';
