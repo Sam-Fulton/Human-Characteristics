79c3596076fd4c4ff09afc439fa98d0178c456de
40293084 <sfulton09@qub.ac.uk>
2023-11-19 17:30:39 +0000

    update mobile drag drop

--- Human-Bias/public/javascript/drag-drop.js
+++ Human-Bias/public/javascript/drag-drop.js
@@ -32,55 +32,58 @@ function dragStart(event) {
 
   function touchStart(event) {
     event.preventDefault();
-    const touchedIndex = event.target.dataset.index;
+    const touchedElement = event.target.closest('[data-index]');
+    const touchedIndex = touchedElement ? touchedElement.dataset.index : null;
   
-    const touch = event.touches[0];
-    event.target.initialTouchX = touch.pageX;
-    event.target.initialTouchY = touch.pageY;
+    if (touchedIndex) {
+      const touch = event.touches[0];
+      touchedElement.initialTouchX = touch.pageX;
+      touchedElement.initialTouchY = touch.pageY;
   
-    event.target.addEventListener('touchmove', touchMove);
-    event.target.addEventListener('touchend', touchEnd);
+      event.target.addEventListener('touchmove', touchMove);
+      event.target.addEventListener('touchend', touchEnd);
   
-    function touchMove(moveEvent) {
-      moveEvent.preventDefault();
-      const touch = moveEvent.touches[0];
+      function touchMove(moveEvent) {
+        moveEvent.preventDefault();
+        const touch = moveEvent.touches[0];
   
-      const deltaX = touch.pageX - event.target.initialTouchX;
-      const deltaY = touch.pageY - event.target.initialTouchY;
+        const deltaX = touch.pageX - touchedElement.initialTouchX;
+        const deltaY = touch.pageY - touchedElement.initialTouchY;
   
-      event.target.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
-    }
-  
-    function touchEnd(endEvent) {
-      endEvent.preventDefault();
-      const releasedIndex = endEvent.target.dataset.index;
-      console.log(releasedIndex);
-  
-      const draggedImage = document.querySelector(`[data-index="${touchedIndex}"]`);
-      console.log("Dragged Image : " + draggedImage)
-      const droppedImage = document.querySelector(`[data-index="${releasedIndex}"]`);
-      console.log("Dropped Image" + droppedImage);
-
-      if (draggedImage && droppedImage) {
-        const parent = draggedImage.parentNode;
-  
-        const temp = document.createElement('div');
-        parent.insertBefore(temp, draggedImage);
-        parent.insertBefore(draggedImage, droppedImage);
-        parent.insertBefore(droppedImage, temp);
-        parent.removeChild(temp);
-  
-        draggedImage.dataset.index = releasedIndex;
-        droppedImage.dataset.index = touchedIndex;
-
-        console.log("RELEASED INDEX :" + releasedIndex);
-        console.log("touchedIndex : " + touchedIndex);
+        touchedElement.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
       }
   
-      event.target.style.transform = '';
-
-      event.target.removeEventListener('touchmove', touchMove);
-      event.target.removeEventListener('touchend', touchEnd);
+      function touchEnd(endEvent) {
+        endEvent.preventDefault();
+        const releasedElement = document.elementFromPoint(endEvent.changedTouches[0].clientX, endEvent.changedTouches[0].clientY);
+        const releasedIndex = releasedElement ? releasedElement.dataset.index : null;
+  
+        console.log("RELEASED INDEX :" + releasedIndex);
+        console.log("touchedIndex : " + touchedIndex);
+  
+        if (releasedIndex && touchedIndex) {
+          const draggedImage = document.querySelector(`[data-index="${touchedIndex}"]`);
+          const droppedImage = document.querySelector(`[data-index="${releasedIndex}"]`);
+  
+          if (draggedImage && droppedImage) {
+            const parent = draggedImage.parentNode;
+  
+            const temp = document.createElement('div');
+            parent.insertBefore(temp, draggedImage);
+            parent.insertBefore(draggedImage, droppedImage);
+            parent.insertBefore(droppedImage, temp);
+            parent.removeChild(temp);
+  
+            draggedImage.dataset.index = releasedIndex;
+            droppedImage.dataset.index = touchedIndex;
+          }
+        }
+  
+        touchedElement.style.transform = '';
+  
+        event.target.removeEventListener('touchmove', touchMove);
+        event.target.removeEventListener('touchend', touchEnd);
+      }
     }
   }
   
