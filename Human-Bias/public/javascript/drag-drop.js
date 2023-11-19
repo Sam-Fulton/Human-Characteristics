6d20f1e3458a724052d50891b1835e0619ee96f0
40293084 <sfulton09@qub.ac.uk>
2023-11-19 18:32:19 +0000

    logging and filtering

--- Human-Bias/public/javascript/drag-drop.js
+++ Human-Bias/public/javascript/drag-drop.js
@@ -34,39 +34,54 @@ function dragStart(event) {
     event.preventDefault();
     const touchedIndex = event.target.dataset.index;
 
-    console.log(touchedIndex);
+    console.log("TOUCHED INDEX: " + touchedIndex);
 
     event.target.addEventListener('touchmove', touchMove, { once: true });
 
     function touchMove(moveEvent) {
-        moveEvent.preventDefault();
-        const draggedIndex = touchedIndex;
-
-        const touch = moveEvent.touches[0];
-        const dropTarget = document.elementFromPoint(touch.clientX, touch.clientY);
-        const droppedIndex = dropTarget.dataset.index;
-
-        console.log(droppedIndex);
-        
-        if (draggedIndex !== droppedIndex) {
-            const draggedImage = document.querySelector(`[data-index="${draggedIndex}"]`);
-            const droppedImage = document.querySelector(`[data-index="${droppedIndex}"]`);
-
-            if (draggedImage && droppedImage) {
-                const parent = draggedImage.parentNode;
-
-                const temp = document.createElement('div');
-                parent.insertBefore(temp, draggedImage);
-                parent.insertBefore(draggedImage, droppedImage);
-                parent.insertBefore(droppedImage, temp);
-                parent.removeChild(temp);
-
-                draggedImage.dataset.index = droppedIndex;
-                droppedImage.dataset.index = draggedIndex;
-            }
-        }
-        document.removeEventListener('touchmove', touchMove);
-    }
+      moveEvent.preventDefault();
+      const draggedIndex = touchedIndex;
+  
+      const touch = moveEvent.touches[0];
+      const x = touch.clientX;
+      const y = touch.clientY;
+  
+      const elementsAtTouch = document.elementsFromPoint(x, y).filter(
+          element => element !== event.target
+      );
+      console.log(elementsAtTouch);
+    
+  
+      const dropTarget = elementsAtTouch.find(
+          element => element.dataset && element.dataset.index
+      );
+        console.log("DROP TARGET: " + dropTarget);
+    
+        if (dropTarget) {
+          const droppedIndex = dropTarget.dataset.index;
+          console.log("DROPPED INDEX " + droppedIndex);
+          
+          if (draggedIndex !== droppedIndex) {
+              const draggedImage = document.querySelector(`[data-index="${draggedIndex}"]`);
+              const droppedImage = document.querySelector(`[data-index="${droppedIndex}"]`);
+  
+              if (draggedImage && droppedImage) {
+                  const parent = draggedImage.parentNode;
+  
+                  const temp = document.createElement('div');
+                  parent.insertBefore(temp, draggedImage);
+                  parent.insertBefore(draggedImage, droppedImage);
+                  parent.insertBefore(droppedImage, temp);
+                  parent.removeChild(temp);
+  
+                  draggedImage.dataset.index = droppedIndex;
+                  droppedImage.dataset.index = draggedIndex;
+              }
+          }
+      }
+  
+      document.removeEventListener('touchmove', touchMove);
+  }
 }
   
   function addDragDropListeners(element) {
