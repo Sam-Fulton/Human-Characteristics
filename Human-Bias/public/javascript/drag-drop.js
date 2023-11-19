f4a3d7de6cfbf3682d13cc4405cbce9000282096
40293084 <sfulton09@qub.ac.uk>
2023-11-19 19:41:11 +0000

    removing logs

--- Human-Bias/public/javascript/drag-drop.js
+++ Human-Bias/public/javascript/drag-drop.js
@@ -34,8 +34,6 @@ function touchStart(event) {
   event.preventDefault();
   const touchedIndex = event.target.dataset.index;
 
-  console.log("TOUCHED INDEX: " + touchedIndex);
-
   event.target.addEventListener('touchmove', (moveEvent) => touchMove(moveEvent, touchedIndex));
   event.target.addEventListener('touchend', touchEnd, { once: true });
 }
@@ -57,17 +55,12 @@ function touchEnd(event) {
   const rankSvgDiv = draggedImage.closest('.rank-svg');
   const images = Array.from(rankSvgDiv.querySelectorAll('embed'));
 
-  console.log("IMAGES: " + images);
 
   let dropTarget;
 
   for (const image of images) {
     if (image !== draggedImage) {
-      console.log("IMAGE: " + image);
       const rect = image.getBoundingClientRect();
-      console.log(x);
-      console.log(y);
-      console.log(rect);
       if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
         dropTarget = image;
         break;
@@ -75,12 +68,8 @@ function touchEnd(event) {
     }
   }
 
-  console.log("Drop target:", dropTarget);
-
   if (dropTarget) {
     const droppedIndex = dropTarget.dataset.index;
-
-    console.log("Valid drop target found:", dropTarget);
 
     const parent = draggedImage.parentNode;
 
@@ -93,10 +82,7 @@ function touchEnd(event) {
     draggedImage.dataset.index = droppedIndex;
     dropTarget.dataset.index = draggedIndex;
 
-    console.log("Images swapped successfully!");
-  } else {
-    console.log("No valid drop target found.");
-  }
+  } 
 }
 
 function addDragDropListeners(element) {
